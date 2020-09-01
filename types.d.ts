f1ebfb2872a8309011c7f840e8700b6513d2eefd
UpperCod <mtrujillo@outlook.cl>
2020-09-01 01:05:27 -0400

    type improvement for ts

--- types.d.ts
+++ types.d.ts
@@ -3,6 +3,6 @@ declare module "@uppercod/request" {
     export type Return = [string, string, IncomingMessage];
     export function request(
         url: string,
-        limitRequest?: number = 5
+        limitRequest?: number
     ): Promise<Return>;
 }
